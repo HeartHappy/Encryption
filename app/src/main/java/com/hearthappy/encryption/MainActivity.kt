3865b5babc307d23094a366ff899f3f994cc1c09
chenrui@vesystem.com <chenrui@vesystem.com>
2020-12-07 11:22:23 +0800

    提交遗漏文件

--- app/src/main/java/com/hearthappy/encryption/MainActivity.kt
+++ app/src/main/java/com/hearthappy/encryption/MainActivity.kt
@@ -2,7 +2,6 @@ package com.hearthappy.encryption
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
